8ce9c49297e0bfc352587f20f5a886bca3edb06e
danila nikitin <danila.n2015@yandex.ru>
2026-03-17 14:28:32 +0300

    fix docker-compose.yml: restart=no for test_app and generation test report in sh/test_app.sh

--- sh/run_test.sh
+++ sh/run_test.sh
@@ -1,2 +1,5 @@
 python -m app.setup
-pytest test/ --cov=app --cov=settings
+pytest test/ --cov=app \
+             --cov=settings \
+             --cov-report=xml:/app/reports/xml \
+             --cov-report=html:/app/reports/html
